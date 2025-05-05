012c817cca8044be906d37403c8df4715b3ec174
DiegoAgudelo4 <diegoagudelo180@gmail.com>
2025-05-05 17:33:49 -0500

    Agregado un nuevo footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { NabvarComponent } from './core/shared/components/nabvar/nabvar.component';
+import { FooterComponent } from "./core/shared/components/footer/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatToolbarModule, NabvarComponent],
+  imports: [RouterOutlet, MatToolbarModule, NabvarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/core/shared/components/footer/footer/footer.component.ts
+++ src/app/core/shared/components/footer/footer/footer.component.ts
@@ -1,11 +1,36 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [
+    CommonModule,
+    MatIconModule,
+    RouterLink
+  ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
+  currentTime: string = '';
+  private timer: any;
+
+  ngOnInit() {
+    this.updateTime();
+    this.timer = setInterval(() => this.updateTime(), 1000);
+  }
+
+  ngOnDestroy() {
+    if (this.timer) {
+      clearInterval(this.timer);
+    }
+  }
+
+  private updateTime() {
+    const now = new Date();
+    this.currentTime = now.toLocaleTimeString('es-CO', { hour12: false });
+  }
 
 }
